62c08329f11bdf358f4de46755639c9f03bf9811
Abdi Berhe <abdiberhe@gmail.com>
2024-02-09 15:08:31 +0300

    fix: Avoid public property

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-
-  // custom set
-  devServer: {
-    public: 'django-wheelsonrent.onrender.com', // Change this to your Render.com subdomain
-  },
   
   // custom set
   configureWebpack: {
+    devServer: {
+      allowedHosts: ['django-wheelsonrent.onrender.com'], // Change this to your Render.com subdomain
+    },
     resolve: {
       fallback: {
         crypto: require.resolve('crypto-browserify'),
